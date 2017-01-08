fe6787344535c160a023c02d2575f6850e9fa78f
Orillion <orillion@rizon.net>
2017-01-08 15:34:52 +0100

    Add iOS workspace

---
+++ iOSBridge/iOSBridge/iOSBridge.h
@@ -0,0 +1,13 @@
+//
+//  iOSBridge.h
+//  iOSBridge
+//
+//  Created by Michiel Hegemans on 08/01/2017.
+//  Copyright © 2017 Bromancer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSBridge : NSObject
+
+@end
